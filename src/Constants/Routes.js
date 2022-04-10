5dd5a77e3449b9bf36f522ea1b9fdbd26fba7077
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-04-10 15:28:43 -0500

    configuracion inicial

---
+++ src/Constants/Routes.js
@@ -0,0 +1,5 @@
+export const HOME_ROUTE = "/"
+
+export const PRODUCT_DETAILS = "/product/:id"
+
+export const ABOUT = "/about"
---
+++ src/components/MainRouter/index.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {HOME_ROUTE, PRODUCT_DETAILS, ABOUT}from "../../Constants/Routes"
+import Navbar from "../NavBar";
+
+
+function Mainrouter () {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path={HOME_ROUTE} element="{<Home />} "/>
+        <Route path={PRODUCT_DETAILS} element="{<CreatePerson />}" />
+        <Route path={ABOUT} element="{}" />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Mainrouter;
