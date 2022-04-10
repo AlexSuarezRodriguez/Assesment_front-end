fca93bba65246bbee739c1242b6ba4cc4cafbb08
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-04-10 16:20:19 -0500

    agregando estilos a NavBar sencillos

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavLink } from "react-router-dom"
 import {HOME_ROUTE, ABOUT} from "../../Constants/Routes"
+import './NavBar.css'
 
 function NavBar  ()  {
   return (
