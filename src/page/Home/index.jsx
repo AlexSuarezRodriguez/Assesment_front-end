1f677882379ad15adfdeb842d447900ef1b564e4
Alexander suarez rodriguez <87741454+AlexSuarezRodriguez@users.noreply.github.com>
2022-04-11 14:00:11 -0500

    manejo de loadig en home y detailProducts (#4)

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,28 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import ProductCard from '../../components/ProductCard';
 import { fetchProduct } from '../../store/actions';
 import Loading from '../../components/Loading';
+import './Home.css';
 
 function Home() {
   const products = useSelector((state) => state.products);
   const dispatch = useDispatch();
+  const [load, setLoad] = useState(true);
 
   useEffect(() => {
     dispatch(fetchProduct());
+    setTimeout(() => {
+      setLoad(false);
+    }, 1500);
   }, []);
 
   return (
     <div>
-      {products.map((product) => (
-        <ProductCard
-          key={product.id}
-          id={product.id}
-          title={product.title}
-          image={product.image}
-        />
-      ))}
-      <Loading />
+      {
+      load
+        ? (<Loading />)
+        : (
+          <div className="contenedor">
+            {
+              products.map((product) => (
+                <ProductCard
+                  key={product.id}
+                  id={product.id}
+                  title={product.title}
+                  image={product.image}
+                />
+              ))
+              }
+          </div>
+        )
+    }
     </div>
   );
 }
--- src/page/ProductDetails/index.jsx
+++ src/page/ProductDetails/index.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import Loading from '../../components/Loading';
 
 function ProductDetails() {
+  const [load, setLoad] = useState(true);
   const product = useSelector((state) => state.oneProduct);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoad(false);
+    }, 1000);
+  }, []);
 
   const {
     title,
@@ -12,20 +20,27 @@ function ProductDetails() {
     image,
     rating: { rate, count },
   } = product;
+
   return (
     <div>
-      {title}
-      ,
-      {price}
-      ,
-      {category}
-      ,
-      {description}
-      ,
-      {rate}
-      ,
-      {count}
-      <img src={image} alt="fgv" />
+      {load
+        ? <Loading />
+        : (
+          <div>
+            {title}
+            ,
+            {price}
+            ,
+            {category}
+            ,
+            {description}
+            ,
+            {rate}
+            ,
+            {count}
+            <img src={image} alt="fgv" />
+          </div>
+        )}
     </div>
   );
 }
