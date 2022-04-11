98082ffc2df45cc7c97f62beda6b27a5780dbb0e
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-04-11 15:11:44 -0500

    maenjo de contador

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import Countdown from 'react-countdown';
 import { fetchOneProduct, restState } from '../../store/actions';
 import { PRODUCT_DETAILS } from '../../Constants/Routes';
+import './ProductCard.css';
 
 function ProductCard(props) {
   const { id, image, title } = props;
   const dispatch = useDispatch();
+  const [isActive, setIsActive] = useState(true);
+  const random = Math.floor(Math.random() * 3) + 1;
 
   const handleLinkClick = () => {
     dispatch(restState());
     dispatch(fetchOneProduct(id));
   };
+  const handleOnComplete = () => {
+    setIsActive(false);
+  };
 
   return (
-    <div>
+    <div className="card-tarjeta">
       <img src={image} alt={title} />
       <p>{title}</p>
-      <Link to={`${PRODUCT_DETAILS}/${id}`} onClick={handleLinkClick}>Go details </Link>
+      <Countdown
+        date={Date.now() + 60000 * random}
+        onComplete={handleOnComplete}
+      />
+
+      <Link
+        className={isActive ? 'active' : 'deactive'}
+        to={`${PRODUCT_DETAILS}/${id}`}
+        onClick={handleLinkClick}
+      >
+        Go details
+      </Link>
     </div>
   );
 }
